757e22d9ac9cabb98cd72b4de876fa599cbb4306
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-09-18 10:48:09 -0400

    Updated from Brython Server: 9/18/2018 10:48:08 AM

--- drawing.py
+++ drawing.py
@@ -12,7 +12,7 @@ noOutline = LineStyle(0,black)
 
 rectangle = RectangleAsset(200,200,noOutline,blue)
 circle = CircleAsset(100,noOutline,green)
-
+triangle = PolygonAsset([(100,200), (350,100), (500,200)],noOutline,red)
 
 
 
@@ -26,4 +26,5 @@ circle = CircleAsset(100,noOutline,green)
 
 Sprite(rectangle,(200,200))
 Sprite(circle,(100,300))
+Sprite(triangle,(300,100))
 App().run()

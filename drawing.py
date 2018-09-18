de9495f4d13af074caaaace7e1208225ced3cf0e
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-09-18 10:41:16 -0400

    Updated from Brython Server: 9/18/2018 10:41:15 AM

--- drawing.py
+++ drawing.py
@@ -1,1 +1,20 @@
+from ggame import *
+
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+yellow = Color(0xFFFF00,1)
+
+
+noOutline = LineStyle(0,black)
+
+rectangle = RectangleAsset(400,400,noOutline,blue)
+
+
+
+
+
+
 

39baf29709f99d4dc6aad7b3ccf37fcdb4132255
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-09-18 10:07:01 -0400

    Updated from Brython Server: 9/18/2018 10:07:00 AM

--- fortune.py
+++ fortune.py
@@ -1,1 +1,30 @@
+from random import randint
+color = input('pick a color: red, blue, green, or yellow')
+
+
+if color=='red':
+    fortune=randint(0,3)
+
+
+if color=='blue':
+    
+
+
+if color=='green':
+    
+
+
+if color=='yellow':
+    
+
+
+
+
+
+
+
+
+
+
+
 

9b19f340f94fef4e3d27994718f429096c9117b8
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-09-28 10:31:36 -0400

    Updated from Brython Server: 9/28/2018 10:31:36 AM

--- explorationday.py
+++ explorationday.py
@@ -11,9 +11,15 @@ CELL_SIZE = 20
 def moveRight(event):
     if yes2.x < (COLS-1)*CELL_SIZE:
         yes2.x += CELL_SIZE
-
-
-
+def moveLeft(event):
+    if yes2.x > 0:
+        yes2.x -= CELL_SIZE
+def moveUp(event):
+    if yes2.y > 0:
+        yes2.y -= CELL_SIZE
+def moveDown(event):
+    if yes2.y < (ROWS-1)*CELL_SIZE:
+        yes2.y += CELL_SIZE
 
 if __name__ == '__main__':
     red = Color(0xFF0000,1)
@@ -22,5 +28,7 @@ if __name__ == '__main__':
     yes2=Sprite(yes)
 
     App().listenKeyEvent('keydown','right arrow',moveRight)
-
+    App().listenKeyEvent('keydown','left arrow',moveLeft)
+    App().listenKeyEvent('keydown','up arrow',moveUp)
+    App().listenKeyEvent('keydown','down arrow',moveDown)
     App().run()

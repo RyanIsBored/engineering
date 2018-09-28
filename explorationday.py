23e7622bde581507572aad7a0aede5316fac85c2
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-09-28 10:30:02 -0400

    Updated from Brython Server: 9/28/2018 10:30:01 AM

--- explorationday.py
+++ explorationday.py
@@ -1,1 +1,26 @@
+from ggame import *
 
+
+
+
+ROWS = 20
+COLS = 40
+CELL_SIZE = 20
+
+
+def moveRight(event):
+    if yes2.x < (COLS-1)*CELL_SIZE:
+        yes2.x += CELL_SIZE
+
+
+
+
+if __name__ == '__main__':
+    red = Color(0xFF0000,1)
+    noOutline = LineStyle(0,black)
+    yes = RectangleAsset(CELL_SIZE,CELL_SIZE,noOutline,red)
+    yes2=Sprite(yes)
+
+    App().listenKeyEvent('keydown','right arrow',moveRight)
+
+    App().run()
